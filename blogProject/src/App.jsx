f0bcd7d9e513c4b907eb3ca54f9738f1a8db5b72
rabbithole333 <theworstvalentine@gmail.com>
2024-10-10 10:54:27 +0900

    post

---
+++ blogProject/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <meta charset="utf-8"></meta>
+      <title>'the' blog</title>
+
+      <h1>Write Anything</h1>
+
+      <div>
+        <textarea id="boxsize" type="text"></textarea>
+      </div>
+
+      <button id="click-button">Post</button>
+    </div>
+  );
+}
+export default App
